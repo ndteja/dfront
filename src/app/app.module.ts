658a28965ca5accbfed64cf5dfe13ddfb6229b8d
tanukusaieesh <tanukusaieesh@gmail.com>
2018-01-10 22:12:13 +0530

    added menu nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatMenuModule, MatButtonModule,MatIconModule,MatCardModule} from '@angular/material' ;
 
 
 import { AppComponent } from './app.component';
+import { MenudemoComponent } from './menudemo/menudemo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenudemoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
